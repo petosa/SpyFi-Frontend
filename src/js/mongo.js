b4478574fb0000cac61d263c606b0a67a6b45ec7
petosa <nick.petosa@gmail.com>
2016-09-11 05:55:56 -0400

    help

--- src/js/mongo.js
+++ src/js/mongo.js
@@ -2,18 +2,21 @@ getAllDocs = function(collection) {
 
 console.log("asd");
 
-  var settings = {
-    "async": true,
-    "url": "http://66.175.209.202:28017/test/chart/",
-    "method": "GET",
-    "headers": {
-      "authorization": "Digest username=\\\"mongo-admin\\\", realm=\\\"mongo\\\", nonce=\\\"abc\\\", uri=\\\"/test/chart/\\\", response=\\\"ffcfdec300561b49b610ac02c5f713fd\\\", opaque=\\\"\\\"",
-    }
-  }
-
-  $.ajax(settings).done(function (response) {
-    console.log("me");
-    console.log(response);
-  });
+$.ajax({
+     url: '66.175.209.202:28017/test/chart',
+     type: 'get',
+     dataType: 'jsonp',
+     jsonp: 'jsonp', // mongod is expecting the parameter name to be called "jsonp"
+     success: function (data) {
+       console.log('success', data);
+     },
+     error: function (XMLHttpRequest, textStatus, errorThrown) {
+       console.log('hhh')
+       console.log('error', errorThrown);
+       console.log(textStatus)
+       console.log(errorThrown)
+       console.log(XMLHttpRequest);
+     }
+   });
 
 }
