175df956a731265fc96bdff20403d824d598f61f
petosa <nick.petosa@gmail.com>
2016-09-11 07:29:41 -0400

    wil

--- src/js/chartGen.js
+++ src/js/chartGen.js
@@ -2,21 +2,22 @@ function sleep (time) {
   return new Promise((resolve) => setTimeout(resolve, time));
 }
 
-data = [[1,4], [2,6]];
+data = [];
 count = data.length;
 var chart;
 chartGen = function() {
         chart = document.createElement('highcharts-chart');
         chart.credits = true;
         chart.type = "areaspline";
-        chart.title = "Network Activity";
+        chart.title = "Network Card Activity";
         chart.xLabel = "Time (seconds)";
+        chart.yLabel = "decibels (dB)";
         chart.setData(data);
         document.currentScript.parentNode.insertBefore(chart, document.currentScript.nextSibling);
         startLoop();
 }
 
-var iFrequency = 1000; // expressed in miliseconds
+var iFrequency = 500; // expressed in miliseconds
 var myInterval = 0;
 
 // STARTS and Resets the loop if any
@@ -27,9 +28,10 @@ function startLoop() {
 
 function doSomething()
 {
-    data.push([++count, Math.floor(Math.random() * 10)]);
-    if (data.length > 6) {
-      data.shift();
-    }
-    chart.setData(data);
+    set = pullData(chart);
+    //data.push([++count, Math.floor(Math.random() * 10)]);
+    //if (data.length > 6) {
+    //  data.shift();
+    //}
+    //chart.setData(set);
 }
--- src/js/mongo.js
+++ src/js/mongo.js
@@ -1,14 +1,24 @@
-getAllDocs = function(collection) {
+
+pullData = function(chart) {
 
 console.log("asd");
 
+onGet = function(data) {
+  rows = data.rows;
+  set = [];
+  for (var i = 0; i < rows.length; i++) {
+    set.push([rows[i].date, rows[i].average]);
+  }
+  chart.setData(set);
+}
+
 $.ajax({
-     url: '66.175.209.202:28017/test/chart',
+     url: 'http://66.175.209.202:28017/dataSets/chart/',
      type: 'get',
      dataType: 'jsonp',
      jsonp: 'jsonp', // mongod is expecting the parameter name to be called "jsonp"
      success: function (data) {
-       console.log('success', data);
+       onGet(data);
      },
      error: function (XMLHttpRequest, textStatus, errorThrown) {
        console.log('hhh')
